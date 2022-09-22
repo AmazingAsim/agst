348de1e6d3f4fdb904004ed02110a87aea38c6c3
asim sheikh <amazingasim3000@gmail.com>
2022-09-22 14:57:15 +0530

    compleated..

--- src/app/components/test29/test29.component.ts
+++ src/app/components/test29/test29.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test29.component.css']
 })
 export class Test29Component implements OnInit {
+features=[
+  {title:'TIMELY',desc:'Course Completion'},
+  {title:'24/7',desc:'Doubt Clearing & Faculty Support'},
+  {title:'2 REVISIONS',desc:'and 6 mock Tests'},
+  {title:'DIGITAL LIBRARY',desc:'facility at all Centers'},
+  {title:'HOSTEL',desc:'Facility'},
+  {title:'HI-TECH',desc:'Classrooms with Audio Visual Aids'},
+]
 
+OnlineSupport=[
+  {title:'ONLINE',desc:'Video Lectures'},
+  {title:'RANK TEST',desc:'Series'},
+  {title:'FB LIVE',desc:'Doubt Clearing Sessions'},
+  {title:'UNLIMITED' ,desc:'Practice Questions'},
+  {title:'PERFORMANCE',desc:'Analytics'},
+  {title:'SHORT NOTES',desc:'With Examples'}
+]
   constructor() { }
 
   ngOnInit(): void {
